bd00b2b398b28224ced7ee98414a28af8b3bf357
henri <henri1i.dev@gmail.com>
2022-05-17 20:07:55 -0300

    implement listing and creating features

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="flex flex-row items-center gap-x-4">
+        <label
+            :for="`todo-${todo.id}`" 
+            class="todo-title"
+            :class="{ 'todo-title-checked': isCompleted }"
+        >
+            <input
+                :id="`todo-${todo.id}`"
+                :name="`todo-${todo.id}`"
+                type="checkbox"
+                class="todo-input"
+                v-model="isCompleted"
+            />
+            {{ todo.title }}
+        </label>
+    </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+    name: 'TodoItem',
+
+    props: ['todo'],
+
+    data: function () {
+        return {
+            isCompleted: false
+        }
+    },
+
+    created() {
+        this.isCompleted = Boolean(this.todo.completed_at);
+    },
+
+    updated() {
+        const isCompleted = this.isCompleted
+            ? moment().format()
+            : null;
+
+        this.$emit('updatedCheck', {
+            ...this.todo,
+            completed_at: isCompleted,
+        })
+    }
+}
+</script>
+
+<style scoped>
+    .todo-title {
+        @apply font-medium text-neutral-200 select-none py-4
+    };
+
+    .todo-title-checked {
+        @apply text-neutral-300 line-through
+    };
+
+    .todo-input {
+        @apply focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-800 rounded
+    };
+</style>
